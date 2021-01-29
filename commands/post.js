0d9f59a385dfd1fcdd8a17f1ac62a56a4320691f
ephrem-adugna <ephremadugna1219@gmail.com>
2021-01-29 10:14:16 -0800

    Added Cat Chooser for Meme Command

--- commands/post.js
+++ commands/post.js
@@ -4,24 +4,48 @@ var snoowrap = require('snoowrap');
 module.exports = {
     name: '-meme',
     description: 'Gets a random meme from the r/Genshin_Memepact',
-    command: '`!genshin-meme`',
+    command: '`!genshin-meme category(new, hot, top)` category is optional',
     async execute(message, args, client) {
 
-            const r = new snoowrap({
-                userAgent: 'Test',
-                clientId: 'eaHYq0clSyXQ8Q',
-                clientSecret: 'ApCB0Fnd1Xej8f9qFHkrzKGIkHRD-g',
-                refreshToken: '381619679561-yAfvvTyIDoVTIgB_2N6dyTyc8eIcLQ'
-            });
-            var content;
+        const r = new snoowrap({
+            userAgent: 'Test',
+            clientId: 'eaHYq0clSyXQ8Q',
+            clientSecret: 'ApCB0Fnd1Xej8f9qFHkrzKGIkHRD-g',
+            refreshToken: '381619679561-yAfvvTyIDoVTIgB_2N6dyTyc8eIcLQ'
+        });
+        var content;
         var title;
-         
-        r.getSubreddit("Genshin_Memepact").getNew().then(async post => {
-                        title = post[0].title;
-                        content = post[0].url;
-                        sendMessage(content, title);
-                    });
-            
+        if (args[0] == undefined) args[0] = "";
+        switch (args[0].toLowerCase()) {
+            case "new":
+                (await r.getSubreddit("Genshin_Memepact")).getNew().then(async post => {
+                    title = post[0].title;
+                    content = post[0].url;
+                    sendMessage(content, title);
+                });
+                break;
+            case "hot":
+                (await r.getSubreddit("Genshin_Memepact")).getHot().then(async post => {
+                    title = post[1].title;
+                    content = post[1].url;
+                    sendMessage(content, title);
+                });
+                break;
+            case "top":
+                (await r.getSubreddit("Genshin_Memepact")).getTop().then(async post => {
+                    title = post[0].title;
+                    content = post[0].url;
+                    sendMessage(content, title);
+                });
+                break;
+            default:
+                (await r.getSubreddit("Genshin_Memepact")).getNew().then(async post => {
+                    title = post[0].title;
+                    content = post[0].url;
+                    sendMessage(content, title);
+                });
+                break;
+    }
 
             function sendMessage(description, name) {
                 const answerEmbed = new Discord.MessageEmbed()
